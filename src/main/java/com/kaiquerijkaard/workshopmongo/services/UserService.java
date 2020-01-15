bfdcd35ec19d35f024d05f64670747f9b8c42003
KaiqueRij <kaiquerijkaardcc@gmail.com>
2020-01-15 17:03:32 -0300

    Remocao de usuario com DELETE

--- src/main/java/com/kaiquerijkaard/workshopmongo/services/UserService.java
+++ src/main/java/com/kaiquerijkaard/workshopmongo/services/UserService.java
@@ -30,6 +30,11 @@ public class UserService {
 		return this.userRepository.insert(obj);
 	}
 	
+	public void delete(String id) {
+		findById(id);
+		this.userRepository.deleteById(id);
+	}
+	
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
