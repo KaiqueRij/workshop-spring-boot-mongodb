722775c7a7d5a3359f0786d9e95d2df5d7a65753
KaiqueRij <kaiquerijkaardcc@gmail.com>
2020-01-17 12:47:03 -0300

    Consulta simples com @Query

--- src/main/java/com/kaiquerijkaard/workshopmongo/services/PostService.java
+++ src/main/java/com/kaiquerijkaard/workshopmongo/services/PostService.java
@@ -22,6 +22,6 @@ public class PostService {
 	}
 	
 	public List<Post> findByTitle(String text) {
-		return this.postRepository.findByTitleContainingIgnoreCase(text);
+		return this.postRepository.findByTitle(text);
 	}
 }
