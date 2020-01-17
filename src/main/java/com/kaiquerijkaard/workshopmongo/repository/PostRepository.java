8481836985e6fad8e16780f56bd47c05742ce577
KaiqueRij <kaiquerijkaardcc@gmail.com>
2020-01-17 12:40:16 -0300

    Consulta simples com query methods

--- src/main/java/com/kaiquerijkaard/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/kaiquerijkaard/workshopmongo/repository/PostRepository.java
@@ -1,8 +1,12 @@
 package com.kaiquerijkaard.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.kaiquerijkaard.workshopmongo.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String>{
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
